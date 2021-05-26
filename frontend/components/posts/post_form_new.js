956ff87937f296d90315ba34c322b3a35b262f3d
App Academy Student <lxg073000@gmail.com>
2021-05-26 18:48:16 -0400

    new post styling

--- frontend/components/posts/post_form_new.js
+++ frontend/components/posts/post_form_new.js
@@ -8,36 +8,19 @@ class post_form_new extends Component {
     };
   }
 
-  componentDidMount() {
-    this.props.fetchPosts();
-    this.props.fetchRoutes();
-  }
-
   submitPost() {
     const post = {
       post: {
         user_id: currentUser.id,
-        google_route_id: document
-          .getElementById("supply-routes")
-          .value.split(",")[1],
-        date_completed: document.getElementById("date-val").value,
-        travelMode: document
-          .getElementById("supply-routes")
-          .value.split(",")[0],
-        title: document.getElementById("title-val").value,
-        description: document.getElementById("description-val").value,
-        start_time: document.getElementById("start-val").value,
-        end_time: document.getElementById("end-val").value,
+        username: currentUser.username,
+        title: document.getElementById("new-post-title-val").value,
+        body: document.getElementById("new-post-body-val").value,
       },
     };
     this.props.createPost(post);
   }
 
   render() {
-    const routeNames = [];
-    for (const [key, value] of Object.entries(this.props.routes)) {
-      routeNames.push(value);
-    }
 
     return (
       <div className="component-container-main">
@@ -46,55 +29,26 @@ class post_form_new extends Component {
             <h1 className="bold">New Post</h1>
           </header>
           <main className="post-details">
-            <section className="left">
-              <h2 className="bold">Title</h2>
-              <input id="title-val" type="text" className="text" />
-              <h2 className="bold">Description</h2>
-              <input id="description-val" type="text" className="text" />
-              <div className="time">
-                <div className="time-item">
-                  <h2 className="bold">Date Completed</h2>
-                  <input id="date-val" type="date" />
-                </div>
-                <div className="time-item">
-                  <h2 className="bold">Start Time</h2>
-                  <input id="start-val" type="time" />
-                </div>
-                <div className="time-item">
-                  <h2 className="bold">End Time</h2>
-                  <input id="end-val" type="time" />
-                </div>
-              </div>
-              <h2
-                className="button-orange"
-                onClick={() => this.submitPost()}
-              >
-                Save Post
-              </h2>
-            </section>
-            <section className="right">
-              <div className="supply-route-select">
-                <select id="supply-routes" name="supply-routes">
-                  <option value="SELECT ROUTE">SELECT ROUTE</option>
-                  {routeNames.map((route, idx) => (
-                    <option key={idx} value={`${route.travelMode},${route.id}`}>
-                      {route.name}
-                    </option>
-                  ))}
-                </select>
-                {document.getElementById("supply-routes") ? (
-                  <i
-                    className={
-                      document
-                        .getElementById("supply-routes")
-                        .value.split(",")[1]
-                    }
-                  ></i>
-                ) : null}
-              </div>
-
-              <div className="supply-route-map"></div>
-            </section>
+          <section className="heading">
+          <div className="heading-left">
+            <div className="profile-pic">
+              <img src={window.user_pic} alt="user_pic" />
+            </div>
+            <div className="heading-title">
+              <p>Posting As</p>
+              <h1>{currentUser.username}</h1>
+            </div>
+          </div>
+            <div className="heading-right">
+              <p className="button-white" onClick={()=>location.assign("#/dashboard")}>Discard</p>
+              <p className="button-orange" onClick={()=>this.submitPost()}>Publish</p>
+            </div>
+          </section>
+          <section className="post-input">
+            <input type="text" id="new-post-title-val" className="text" placeholder="Add a title (optional)"/>
+            <textarea id="new-post-body-val" className="textarea" placeholder="What's going on?"/>
+          </section>
+           
           </main>
         </div>
       </div>
