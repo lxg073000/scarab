461ec2a9d01436e8bb0d0aaf8022fcf5073dc57c
App Academy Student <lxg073000@gmail.com>
2021-09-01 08:26:08 -0400

    remove template associations from post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :user_id, :username, :body, presence: true
 
   belongs_to :user
-  has_many :likes, dependent: :destroy
+  # has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one :activity
   belongs_to :google_route, optional: true
