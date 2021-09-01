fddbad9d787dc02777860e7d01ee2110737ae5fe
App Academy Student <lxg073000@gmail.com>
2021-09-01 08:29:02 -0400

    remove template associations from comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
   validates :body, :post_id, :user_id, presence: true
 
   belongs_to :post
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  # has_many :likes, dependent: :destroy
+  # has_many :comments, dependent: :destroy
   belongs_to :user
 
 end
